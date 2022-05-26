a06243ffbae99f3c709523cc44e8ec44f4979c3d
KinkyArielle <ariellesteinlechner@gmail.com>
2022-05-26 04:51:36 +0200

    Changed File Idea

--- download.py
+++ download.py
@@ -5,10 +5,17 @@ hi2 = "Hiii"
 hi3 = 'test'
 
 
-#git add .  #Fügt die Veränderungen dazu
+        #Log Stuff
 #git diff . #Zeigt die Änderungen 
 #git log . #Zeigt die Änderungs History
-#git push #pushed to Git Repo.
 
 
+
+        #Instruct on how to apply changes to the Repository
+#git add .  #Fügt die Veränderungen dazu
+#git commit -m "Hier Kommt Ein Kommentar Dazu"
+#git push   #pushed to Git Repo.
+
+
+        #Terminal Helpfull Information
 #get out of the terminal = Q Button 
