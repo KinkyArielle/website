5a38279d2842f02ec69060e3afae79e2d4c303d9
KinkyArielle <ariellesteinlechner@gmail.com>
2022-05-26 04:45:21 +0200

    Hello World Terminal Kommentierung

--- download.py
+++ download.py
@@ -1,1 +1,14 @@
 hello = "hi"
+
+
+hi2 = "Hiii"
+hi3 = 'test'
+
+
+#git add .  #Fügt die Veränderungen dazu
+#git diff . #Zeigt die Änderungen 
+#git log . #Zeigt die Änderungs History
+#git push #pushed to Git Repo.
+
+
+#get out of the terminal = Q Button 
